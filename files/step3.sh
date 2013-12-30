9079cf6afe176b7f6e014a9553e6b75154d912e3
Rachit Rawat <rachit.rc96@gmail.com>
2013-12-30 17:36:58 +0530

    1.0

---
+++ files/step3.sh
@@ -0,0 +1,23 @@
+#!/data/local/tmp/cwm/sh
+
+echo remount rw /system
+mount -o remount rw /system
+
+echo copy recovery.tar to system.
+cat /data/local/tmp/cwm/recovery.tar > /system/bin/recovery.tar
+chmod 644 /system/bin/recovery.tar
+
+echo copy script to system.
+cat /data/local/tmp/cwm/install-recovery.sh > /system/etc/install-recovery.sh
+chmod 755 /system/etc/install-recovery.sh
+
+echo copy binary to system.
+cat /data/local/tmp/cwm/sa77_recovery > /system/bin/sa77_recovery
+chmod 755 /system/bin/sa77_recovery
+
+echo copy sh to system.
+cat /data/local/tmp/cwm/sh > /system/xbin/sh
+chmod 755 /system/xbin/sh
+
+echo remount ro /system
+mount -o remount ro /system
