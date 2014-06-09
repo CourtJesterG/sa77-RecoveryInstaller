3d25970a2ce8f019d8c988ed536bba449fd659bb
rachit-rc <rachit.rc.rawat@gmail.com>
2014-06-09 12:57:35 +0530

    Switch to menu drive
    New features include installing some other recovery.tar and uninstalling existing recovery. Plus some UI enhancements.

--- install.sh
+++ install.sh
@@ -1,19 +1,37 @@
-echo "** CWM for Xperia L - Rachit Rawat and [NUT] **"
+setterm -bold
 
+echo "** Recovery Installer for Xperia L - v1.4**"
+echo "**    By Rachit Rawat and [NUT]          **"
+echo 
+
+echo
+echo ===============================================
+echo Step1 : Connect device with usb debugging on...
+echo ===============================================
+adb wait-for-device
+echo Device Detected.
+echo
+
+while :
+do
+tput setaf 6
+setterm -bold
+echo "1. Install CWM 6.0.4.8"
+echo "2. Install some other recovery"
+echo "3. Uninstall existing recovery"
+echo "4. Exit"
+tput sgr0
+read ANS
+
+case $ANS in
+1) 
 cd files
 
 adb kill-server
 adb start-server
 
 echo =============================================
-echo Step1 : Waiting for Device...
-echo =============================================
-adb wait-for-device
-echo Device Detected.
-echo .
-
-echo =============================================
-echo Step2 : Sending files...
+echo Step2 : Installing recovery...
 echo =============================================
 adb shell "mkdir /data/local/tmp/cwm"
 adb push recovery.sh /data/local/tmp/cwm
@@ -21,11 +39,6 @@ adb push e2fsck.sh /data/local/tmp/cwm
 adb push recovery.tar /data/local/tmp/cwm
 adb push busybox /data/local/tmp/cwm
 adb push step3.sh /data/local/tmp/cwm
-
-echo .
-echo =============================================
-echo Step3 : Setting up files...
-echo =============================================
 adb shell "chmod 755 /data/local/tmp/cwm/busybox"
 adb shell "chmod 755 /data/local/tmp/cwm/step3.sh"
 adb shell "su -c /data/local/tmp/cwm/step3.sh"
@@ -33,4 +46,57 @@ adb shell "rm -r /data/local/tmp/cwm"
 
 adb kill-server
 
+echo
 echo Finished!
+echo
+;;
+
+2) 
+cd files
+if ! test -d ../input
+then mkdir ../input
+fi
+
+echo Place your recovery.tar in input folder and press enter.
+read ANS2
+if test  -e ../input/recovery.tar
+   then echo "Recovery found."
+echo =============================================
+echo Step2 : Installing recovery...
+echo =============================================
+adb shell "mkdir /data/local/tmp/cwm"
+adb push recovery.sh /data/local/tmp/cwm
+adb push e2fsck.sh /data/local/tmp/cwm
+adb push ../input/recovery.tar /data/local/tmp/cwm
+adb push busybox /data/local/tmp/cwm
+adb push step3.sh /data/local/tmp/cwm
+adb shell "chmod 755 /data/local/tmp/cwm/busybox"
+adb shell "chmod 755 /data/local/tmp/cwm/step3.sh"
+adb shell "su -c /data/local/tmp/cwm/step3.sh"
+adb shell "rm -r /data/local/tmp/cwm"
+echo 
+echo Finished!
+else echo "Recovery not found!"
+   exit 
+fi
+;;
+
+3) echo "Uninstalling recovery.."
+cd files
+adb shell "mkdir /data/local/tmp/cwm"
+adb push busybox /data/local/tmp/cwm
+adb push unin.sh /data/local/tmp/cwm
+adb shell "chmod 755 /data/local/tmp/cwm/busybox"
+adb shell "chmod 755 /data/local/tmp/cwm/unin.sh"
+adb shell "su -c /data/local/tmp/cwm/unin.sh"
+adb shell "rm -r /data/local/tmp/cwm"
+echo 
+echo "Finished!"
+;;
+
+4) 
+exit
+;;
+
+esac
+done
